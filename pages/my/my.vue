058ce5491641cc4841f869c52850959fd1b8c8e7
dreamisfarfly <dreamisfarfly@gmail.com>
2022-09-16 14:54:01 +0800

    main

--- pages/my/my.vue
+++ pages/my/my.vue
@@ -1,11 +1,89 @@
 <template>
   <!-- start 我的 -->
-  <view class="my"></view>
+  <view>
+    <!-- start my-header -->
+    <view class="my-header">
+      <!-- start my-header-user-info -->
+      <view class="my-header-user-info">
+        <!-- start user-info -->
+        <view class="user-info">
+          <img class="user-buddha" src="" />
+          <view class="info">
+            <view class="user-name-vessel">
+              <text class="user-name">商户名丽丽</text>
+              <img
+                class="shopkeeper-icon"
+                src="/static/images/shopkeeper-icon.png"
+              />
+            </view>
+            <view class="user-account">账号：534536</view>
+          </view>
+        </view>
+        <!-- end user-info -->
+        <!-- start ewm-icon -->
+        <img class="ewm-icon" src="/static/images/ewm-icon.png" />
+        <!-- end ewm-icon -->
+      </view>
+      <!-- end my-header-user-info -->
+      <!-- start data-info -->
+      <view class="data-info">
+        <view class="data-info-operation">
+          <text class="complete-information">资料完善度 50%</text>
+          <text class="to-perfect">去完善>></text>
+        </view>
+        <view class="progress-bar-vessel">
+          <view class="progress-bar">
+            <view
+              class="schedule"
+              :style="{ width: scheduleCount + '%' }"
+            ></view>
+          </view>
+        </view>
+      </view>
+      <!-- end data-info -->
+    </view>
+    <!-- end my-header -->
+    <view class="my-content">
+      <!-- start user-detailed -->
+      <view class="user-detailed">
+        <!-- start member-info -->
+        <view class="member-info">
+          <view class="member-info-list">
+            <view class="member-info-list-count">12个</view>
+            <view class="member-info-list-vip-name">门店会员</view>
+          </view>
+          <view class="member-info-list">
+            <view class="member-info-list-count">130个</view>
+            <view class="member-info-list-vip-name">直属会员</view>
+          </view>
+        </view>
+        <!-- end member-info -->
+        <!-- start commission -->
+        <view class="commission">
+          <view class="commission-info">
+            <view class="commission-info-count">2000.00</view>
+            <view class="commission-info-name">我的佣金</view>
+          </view>
+          <view class="commission-operation">
+            <view class="withdrawal-record">提现记录</view>
+            <view class="to-withdrawal">去提现</view>
+          </view>
+        </view>
+        <!-- start commission -->
+      </view>
+      <!-- end user-detailed -->
+    </view>
+  </view>
   <!-- end 我的 -->
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      scheduleCount: 20, //进度 %
+    };
+  },
   mounted() {
     var a = document.getElementsByClassName("uni-page-head-hd")[0];
     a.style.opacity = 0;
@@ -14,6 +92,183 @@ export default {
 </script>
 
 <style scoped lang="scss">
-.my {
+.my-header {
+  width: 100%;
+  height: 454rpx;
+  background: url("/static/images/my-header-bak.png");
+  background-size: 100% 100%;
+  padding: 0 32rpx;
+  padding-top: 86rpx;
+  box-sizing: border-box;
+  .my-header-user-info {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    .user-info {
+      display: flex;
+      .user-buddha {
+        width: 136rpx;
+        height: 136rpx;
+        border-radius: 50%;
+      }
+      .info {
+        margin-left: 22rpx;
+        .user-name-vessel {
+          margin-top: 12rpx;
+          display: flex;
+          align-items: center;
+          .user-name {
+            font-size: 32rpx;
+            font-weight: 500;
+            color: #ffffff;
+          }
+          .shopkeeper-icon {
+            margin-left: 20rpx;
+            width: 60rpx;
+            height: 30rpx;
+          }
+        }
+        .user-account {
+          margin-top: 12rpx;
+          font-size: 24rpx;
+          font-weight: 400;
+          color: #ffffff;
+        }
+      }
+    }
+    .ewm-icon {
+      width: 50rpx;
+      height: 50rpx;
+      margin-right: 16rpx;
+    }
+  }
+  .data-info {
+    margin-top: 22rpx;
+    width: 100%;
+    background: rgba(#ffffff, 0.2);
+    border-radius: 16rpx;
+    .data-info-operation {
+      padding: 16rpx 30rpx 12rpx 30rpx;
+      box-sizing: border-box;
+      display: flex;
+      justify-content: space-between;
+      color: #ffffff;
+      font-size: 24rpx;
+      .complete-information {
+        font-weight: 400;
+      }
+      .to-perfect {
+        font-weight: 500;
+      }
+    }
+  }
+  .progress-bar-vessel {
+    padding: 12rpx 28rpx 18rpx 24rpx;
+    box-sizing: border-box;
+    .progress-bar {
+      background: #a37a5e;
+      border-radius: 10rpx;
+      width: 100%;
+      height: 20rpx;
+      display: flex;
+      align-items: center;
+      padding: 0 4rpx;
+      box-sizing: border-box;
+      .schedule {
+        height: 12rpx;
+        background: linear-gradient(270deg, #ffffff 0%, #fffcf8 100%);
+        box-shadow: 0 2rpx 2rpx 0 rgba(245, 198, 132, 0.58);
+        border-radius: 12rpx;
+      }
+    }
+  }
+}
+
+.my-content {
+  position: relative;
+  top: -80rpx;
+  width: 100%;
+  padding: 0 30rpx;
+  box-sizing: border-box;
+  .user-detailed {
+    width: 100%;
+    background: #ffffff;
+    box-shadow: 0 -10rpx 12rpx 0 rgba(0, 0, 0, 0.05);
+    border-radius: 16rpx;
+    padding: 20rpx;
+    padding-top: 0rpx;
+    box-sizing: border-box;
+    .member-info {
+      display: flex;
+      .member-info-list {
+        flex: 1;
+        display: flex;
+        align-items: center;
+        flex-direction: column;
+        padding: 40rpx 0;
+        .member-info-list-count {
+          font-size: 40rpx;
+          font-weight: 600;
+          color: #333333;
+        }
+        .member-info-list-vip-name {
+          margin-top: 10rpx;
+          font-size: 24rpx;
+          font-weight: 400;
+          color: #999999;
+        }
+      }
+    }
+    .commission {
+      width: 100%;
+      background: rgba(220, 174, 98, 0.11);
+      border-radius: 16rpx;
+      border: 2rpx solid #ded0bb;
+      padding: 14rpx 24rpx 18rpx 26rpx;
+      box-sizing: border-box;
+      display: flex;
+      justify-content: space-between;
+      .commission-info {
+        .commission-info-count {
+          font-size: 48rpx;
+          font-weight: 600;
+          color: #333333;
+        }
+        .commission-info-name {
+          font-size: 20rpx;
+          font-weight: 500;
+          color: #999999;
+        }
+      }
+      .commission-operation {
+        display: flex;
+        padding-top: 24rpx;
+        .withdrawal-record {
+          width: 128rpx;
+          height: 50rpx;
+          border-radius: 8rpx;
+          border: 2rpx solid #c78125;
+          font-size: 24rpx;
+          font-weight: 500;
+          color: #c78125;
+          text-align: center;
+          line-height: 50rpx;
+        }
+        .to-withdrawal {
+          margin-left: 20rpx;
+          width: 120rpx;
+          height: 50rpx;
+          background: #c78125;
+          border-radius: 8rpx;
+          border: 2rpx solid #c78125;
+          font-size: 24rpx;
+          font-weight: 500;
+          color: #ffffff;
+          text-align: center;
+          line-height: 50rpx;
+        }
+      }
+    }
+  }
 }
 </style>
