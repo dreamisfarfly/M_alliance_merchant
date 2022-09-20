51b3ac6c723cabeeb13f6aad218a5d486a04abbf
dreamisfarfly <dreamisfarfly@gmail.com>
2022-09-20 11:12:06 +0800

    main

---
+++ components/couponCancellationCard/couponCancellationCard.vue
@@ -0,0 +1,126 @@
+<template>
+  <!-- start coupon-cancellation-card -->
+  <view class="coupon-cancellation-card">
+    <!-- start coupon-cancellation-card-header -->
+    <view class="coupon-cancellation-card-header">
+      <!-- start coupon-type -->
+      <view class="coupon-type">
+        <img
+          class="coupon-icon"
+          src="/static/images/discount-coupon-magcard.png"
+        />
+      </view>
+      <!-- end coupon-type -->
+      <view class="coupon-info">
+        <view
+          :class="[
+            scene == 'header' ? 'coupon-info-name-header' : '',
+            scene == 'record' ? 'coupon-info-name-record' : '',
+          ]"
+          >券码：545613213</view
+        >
+        <view
+          class="coupon-info-subheading"
+          :style="[{ fontSize: scene == 'header' ? '24rpx' : '32rpx' }]"
+          >洁牙卡一次</view
+        >
+        <view class="hint" style="margin-top: 10rpx"
+          >有效期至 2022年6月27日</view
+        >
+      </view>
+    </view>
+    <!-- end coupon-cancellation-card-header -->
+    <!-- start coupon-cancellation-card-footer -->
+    <view class="coupon-cancellation-card-footer">
+      <template v-if="scene == 'header'">
+        <view class="hint">平台规则：必须到店使用</view>
+        <view class="hint">门店规则：周六、周日不可用</view>
+      </template>
+      <template v-if="scene == 'record'">
+        <view class="hint">核销门店：巢爷老味（洞井路店）</view>
+        <view class="justify-align" style="margin-top: 14rpx">
+          <view class="hint">核销人：张三</view>
+          <view class="hint">核销时间：2022年5月27日16:18</view>
+        </view>
+      </template>
+    </view>
+    <!-- end coupon-cancellation-card-footer -->
+  </view>
+  <!-- end coupon-cancellation-card -->
+</template>
+
+<script>
+export default {
+  props: {
+    // 场景
+    scene: {
+      typeof: String,
+      default: "header",
+    },
+  },
+};
+</script>
+
+<style lang="less" scoped>
+.coupon-cancellation-card {
+  background: #ffffff;
+  box-shadow: 0 8rpx 16rpx 0 rgba(108, 119, 128, 0.05);
+  border-radius: 10rpx;
+  .coupon-cancellation-card-header {
+    padding: 26rpx 0 20rpx 0;
+    border-bottom: 2rpx dashed #e3e3e3;
+    display: flex;
+    .coupon-type {
+      width: 134rpx;
+      border-right: 2rpx dashed #e3e3e3;
+      padding-top: 26rpx;
+      padding-bottom: 30rpx;
+      display: flex;
+      justify-content: center;
+      .coupon-icon {
+        width: 98rpx;
+        height: 98rpx;
+      }
+    }
+    .coupon-info {
+      flex: 1;
+      padding-left: 16rpx;
+      .coupon-info-name-header {
+        font-size: 40rpx;
+        font-weight: 500;
+        color: #c78125;
+      }
+      .coupon-info-name-record {
+        font-size: 28rpx;
+        font-weight: 500;
+        color: #c7c7c7;
+      }
+      .coupon-info-subheading {
+        margin-top: 20rpx;
+        font-weight: 500;
+        color: #333333;
+      }
+      .coupon-info-at {
+        margin-top: 10rpx;
+        font-size: 20rpx;
+        font-weight: 400;
+        color: #999999;
+      }
+    }
+  }
+  .coupon-cancellation-card-footer {
+    padding: 24rpx 22rpx;
+  }
+}
+
+.hint {
+  font-size: 20rpx;
+  font-weight: 400;
+  color: #999999;
+}
+
+.justify-align {
+  display: flex;
+  justify-content: space-between;
+}
+</style>
---
+++ pages/my/couponCancellation.vue
@@ -0,0 +1,162 @@
+<template>
+  <!-- start coupon-cancellation -->
+  <view class="coupon-cancellation">
+    <!-- start coupon-cancellation-header -->
+    <view class="coupon-cancellation-header">
+      <!-- start 导航栏 -->
+      <u-navbar
+        title="优惠券核销"
+        :is-back="false"
+        title-color="#FFFFFF"
+        title-size="36"
+        :border-bottom="false"
+      ></u-navbar>
+      <!-- end 导航栏 -->
+      <!-- start coupon-cancellation-header-content -->
+      <view class="coupon-cancellation-header-content">
+        <CouponCancellationCard></CouponCancellationCard>
+        <!-- start member-information-card -->
+        <view class="member-information-card">
+          <view class="member-information-name">会员信息</view>
+          <view class="justify">
+            <view class="member-name">
+              张三
+              <view class="member-label">年卡会员</view>
+            </view>
+            <view class="member-info-font"
+              >女<text style="margin-left: 42rpx">15699990000</text></view
+            >
+          </view>
+          <view class="member-info-font" style="margin-top: 10rpx"
+            >有效期至：2022-12-12</view
+          >
+        </view>
+        <!-- end member-information-card -->
+        <!-- start btn-group -->
+        <view class="btn-group">
+          <view class="btn return">返回</view>
+          <view class="btn verification">核销</view>
+        </view>
+        <!-- end btn-group -->
+      </view>
+      <!-- end coupon-cancellation-header-content -->
+    </view>
+    <!-- end coupon-cancellation-header -->
+    <!-- start record -->
+    <view class="record">
+      <view class="record-title">核销记录</view>
+      <view class="record-content">
+        <CouponCancellationCard scene="record"></CouponCancellationCard>
+      </view>
+    </view>
+    <!-- end record -->
+  </view>
+  <!-- end coupon-cancellation -->
+</template>
+
+<script>
+import CouponCancellationCard from "@/components/couponCancellationCard/couponCancellationCard.vue";
+export default {
+  data() {
+    return {};
+  },
+  components: { CouponCancellationCard },
+};
+</script>
+
+<style lang="less" scoped>
+.coupon-cancellation {
+  .coupon-cancellation-header {
+    padding-bottom: 30rpx;
+    width: 100%;
+    background: linear-gradient(270deg, #c9a88b 0%, #996f52 100%);
+    .coupon-cancellation-header-content {
+      padding: 0 30rpx;
+      margin-top: 20rpx;
+      box-sizing: border-box;
+      .member-information-card {
+        padding: 16rpx 26rpx 36rpx 30rpx;
+        border-radius: 10rpx;
+        margin-top: 30rpx;
+        box-sizing: border-box;
+        background: rgba(255, 255, 255, 0.1);
+        box-shadow: 0 8rpx 16rpx 0 rgba(108, 119, 128, 0.05);
+        border: 2rpx solid;
+        border-image: linear-gradient(
+            180deg,
+            rgba(255, 255, 255, 1),
+            rgba(255, 255, 255, 0)
+          )
+          1 1;
+        .member-information-name {
+          font-size: 24rpx;
+          font-weight: 400;
+          color: #ffffff;
+        }
+        .justify {
+          margin-top: 24rpx;
+          display: flex;
+          align-items: center;
+          justify-content: space-between;
+        }
+        .member-name {
+          display: flex;
+          font-size: 24rpx;
+          font-weight: 500;
+          color: #ffffff;
+        }
+        .member-info-font {
+          font-size: 24rpx;
+          font-weight: 400;
+          color: #ffffff;
+        }
+      }
+      .btn-group {
+        display: flex;
+        justify-content: space-between;
+        margin-top: 34rpx;
+        .btn {
+          width: 300rpx;
+          height: 72rpx;
+          border-radius: 16rpx;
+          text-align: center;
+          line-height: 72rpx;
+          font-size: 32rpx;
+          font-weight: 600;
+        }
+        .return {
+          border: 2rpx solid #ffffff;
+          color: #ffffff;
+        }
+        .verification {
+          color: #c78125;
+          background: #ffffff;
+        }
+      }
+    }
+  }
+}
+
+.record {
+  margin-top: 36rpx;
+  padding: 0 30rpx;
+  .record-title {
+    font-size: 28rx;
+    font-weight: 600;
+    color: #333333;
+  }
+  .record-content {
+    margin-top: 18rpx;
+  }
+}
+
+.member-label {
+  margin-left: 26rpx;
+  padding: 2rpx 14rpx 2rpx 18rpx;
+  background: #528a13;
+  border-radius: 4rpx;
+  font-size: 20rpx;
+  font-weight: 500;
+  color: #ffffff;
+}
+</style>
