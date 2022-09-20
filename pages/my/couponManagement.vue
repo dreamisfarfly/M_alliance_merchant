6dbe87f1cd8b945eec9771fb593c0d133830ed6e
dreamisfarfly <dreamisfarfly@gmail.com>
2022-09-20 13:56:57 +0800

    main

--- pages/my/couponManagement.vue
+++ pages/my/couponManagement.vue
@@ -19,58 +19,22 @@
     <!-- start 切换栏 -->
     <!-- start coupon-management-content -->
     <view class="coupon-management-content">
-      <!-- start coupon-management-list -->
-      <view class="coupon-management-list" v-for="(item, key) in couponList" :key="key">
-        <!-- start coupon-management-type-card -->
-        <view class="coupon-management-type-card">
-          <img
-            v-if="item.type == 0"
-            class="coupon-management-type-icons"
-            src="/static/images/discount-coupon-magcard.png"
-          />
-          <img
-            v-if="item.type == 1"
-            class="coupon-management-type-icons"
-            src="/static/images/discount-coupon-consumer-voucher.png"
-          />
-          <img
-            v-if="item.type == 2"
-            class="coupon-management-type-icons"
-            src="/static/images/discount-coupon-cash-coupon.png"
-          />
-        </view>
-        <!-- end coupon-management-type-card -->
-        <!-- start coupon-management-info-card -->
-        <view class="coupon-management-info-card">
-          <!-- start coupon-management-info-card-header -->
-          <view class="coupon-management-info-card-header">
-            <view class="coupon-management-info-card-header-name"
-              >{{item.title}}</view
-            >
-            <view class="addition-btn" v-if="swiperCurrent == 0">下架</view>
-            <view class="addition-btn" v-if="swiperCurrent == 1">添加</view>
-          </view>
-          <!-- end coupon-management-info-card-header -->
-          <!-- start coupon-management-info-card-value -->
-          <view class="coupon-management-info-card-value">
-            <template v-if="item.type==0">价值 188元</template>
-            <template v-if="item.type==1">满100减10元现金券</template>
-            <template v-if="item.type==2">现金直接抵扣</template>
-          </view>
-          <!-- end coupon-management-info-card-value -->
-          <!-- start coupon-management-info-card-separator -->
-          <view class="coupon-management-info-card-separator"></view>
-          <!-- end coupon-management-info-card-separator -->
-          <!-- start coupon-management-info-card-expiration-time -->
-          <view class="coupon-management-info-card-expiration-time">
-            <text>领取后30天有效</text>
-            <text v-if="swiperCurrent == 0">10家门店适用></text>
-          </view>
-          <!-- end coupon-management-info-card-expiration-time -->
-        </view>
-        <!-- end coupon-management-info-card -->
-      </view>
-      <!-- end coupon-management-list -->
+      <template v-if="swiperCurrent == 0">
+        <CouponCard
+          scene="listShow"
+          :coupon="item"
+          v-for="(item, key) in couponList"
+          :key="key"
+        />
+      </template>
+      <template v-if="swiperCurrent == 1">
+        <CouponCard
+          scene="listAdd"
+          :coupon="item"
+          v-for="(item, key) in couponList"
+          :key="key"
+        />
+      </template>
     </view>
     <!-- end coupon-management-content -->
   </view>
@@ -78,11 +42,12 @@
 </template>
 
 <script>
+import CouponCard from "@/components/couponCard/couponCard.vue";
 export default {
   data() {
     return {
       //  swiper组件的current值
-      swiperCurrent: 0, //0:优惠劵列表 1:添加优惠劵
+      swiperCurrent: 0,
       // tab栏滑块样式
       tableBarStyle: {
         background: "linear-gradient(270deg, #D99A48 0%, #F5C684 100%)",
@@ -123,6 +88,7 @@ export default {
     var a = document.getElementsByClassName("uni-page-head-hd")[0];
     a.style.opacity = 0;
   },
+  components: { CouponCard },
 };
 </script>
 
@@ -130,73 +96,5 @@ export default {
 .coupon-management-content {
   padding: 0 14rpx;
   padding-top: 12rpx;
-  .coupon-management-list {
-    width: 100%;
-    height: 240rpx;
-    background: url("/static/images/discount-coupon-bak.png");
-    background-size: 100% 100%;
-    display: flex;
-    .coupon-management-type-card {
-      width: 152rpx;
-      height: 100%;
-      padding-left: 30rpx;
-      box-sizing: border-box;
-      display: flex;
-      align-items: center;
-      .coupon-management-type-icons {
-        width: 98rpx;
-        height: 106rpx;
-      }
-    }
-    .coupon-management-info-card {
-      flex: 1;
-      padding: 6rpx 46rpx 18rpx 16rpx;
-      box-sizing: border-box;
-      .coupon-management-info-card-header {
-        padding-top: 24rpx;
-        display: flex;
-        align-items: center;
-        justify-content: space-between;
-        box-sizing: border-box;
-        .coupon-management-info-card-header-name {
-          font-size: 32rpx;
-          font-weight: 500;
-          color: #333333;
-        }
-        .addition-btn {
-          width: 120rpx;
-          height: 50rpx;
-          border-radius: 8rpx;
-          border: 2rpx solid #c78125;
-          text-align: center;
-          line-height: 50rpx;
-          box-sizing: border-box;
-          font-size: 24rpx;
-          font-weight: 500;
-          color: #c78125;
-        }
-      }
-    }
-    .coupon-management-info-card-value {
-      margin-top: 16rpx;
-      font-size: 24rpx;
-      font-weight: 400;
-      color: #999999;
-    }
-    .coupon-management-info-card-separator {
-      margin-top: 22rpx;
-      width: 100%;
-      height: 2rpx;
-      background: #f0f0f0;
-    }
-    .coupon-management-info-card-expiration-time {
-      margin-top: 18rpx;
-      font-size: 20rpx;
-      font-weight: 400;
-      color: #999999;
-      display: flex;
-      justify-content: space-between;
-    }
-  }
 }
 </style>
